55e6a0c19213f86d26887ef0c68ee0645d04a814
Hassan13102003 <115918799+Hassan13102003@users.noreply.github.com>
2024-02-19 09:03:34 -0800

    Add files via upload
    In this code i have implemented ArrayLists in java which is like vectors in C++.It also includes some of the Questions involved in it.

---
+++ ArrayList.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ArrayList {
+public static void main(String[] args) {
+    ArrayList<Interger> height=new ArrayList<>();
+    height.add(1);
+    height.get(0);
+}
+}
